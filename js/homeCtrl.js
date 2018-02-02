dafa0a4882462d09ccee4d5c82e7c0888384c5a0
Xiao Peng <peng.xiao@dianrong.com>
2018-02-02 17:28:12 +0800

    换到自己的电脑上面继续。

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -1,41 +1,4 @@
 andyApp.controller('homeCtrl', function($scope) {
     console.info("test");
-    $scope.articles = {
-        "Spring": [
-            {
-                "name": "Spring中的循环依赖",
-                "src" : "http://www.baidu.com",
-                "shortDesc": "",
-                "createdDate": 123
-            },{
-                "name": "Spring中的循环依赖2",
-                "src" : "http://www.baidu.com",
-                "shortDesc": "",
-                "createdDate": 123
-            },{
-                "name": "Spring中的循环依赖3",
-                "src" : "http://www.baidu.com",
-                "shortDesc": "",
-                "createdDate": 123
-            }
-        ],
-        "Java" : [
-            {
-                "name": "Spring中的循环依赖",
-                "src" : "http://www.baidu.com",
-                "shortDesc": "",
-                "createdDate": 123
-            },{
-                "name": "Spring中的循环依赖2",
-                "src" : "http://www.baidu.com",
-                "shortDesc": "",
-                "createdDate": 123
-            },{
-                "name": "Spring中的循环依赖3",
-                "src" : "http://www.baidu.com",
-                "shortDesc": "",
-                "createdDate": 123
-            }
-        ]
-    };
+
 });
--- js/initCtrl.js
+++ js/initCtrl.js
@@ -1,3 +1,66 @@
-andyApp.controller('initCtrl', function($scope, $http) {
-    console.info("asdasd");
+andyApp.controller('initCtrl', function ($scope, $http, $rootScope) {
+    $rootScope.articles = {
+        "Spring": [
+            {
+                "name": "Spring中的循环依赖",
+                "src": "http://www.baidu.com",
+                "shortDesc": "",
+                "createdDate": 123
+            }, {
+                "name": "Spring中的循环依赖2",
+                "src": "http://www.baidu.com",
+                "shortDesc": "",
+                "createdDate": 123
+            }, {
+                "name": "Spring中的循环依赖3",
+                "src": "http://www.baidu.com",
+                "shortDesc": "",
+                "createdDate": 123
+            }
+        ],
+        "Java": [
+            {
+                "name": "Spring中的循环依赖",
+                "src": "http://www.baidu.com",
+                "shortDesc": "",
+                "createdDate": 123
+            }, {
+                "name": "Spring中的循环依赖2",
+                "src": "http://www.baidu.com",
+                "shortDesc": "",
+                "createdDate": 123
+            }, {
+                "name": "Spring中的循环依赖3",
+                "src": "http://www.baidu.com",
+                "shortDesc": "",
+                "createdDate": 123
+            }
+        ],
+        "JDK": [
+            {
+                "name": "Spring中的循环依赖",
+                "src": "http://www.baidu.com",
+                "shortDesc": "",
+                "createdDate": 123
+            }, {
+                "name": "Spring中的循环依赖2",
+                "src": "http://www.baidu.com",
+                "shortDesc": "",
+                "createdDate": 123
+            }, {
+                "name": "Spring中的循环依赖3",
+                "src": "http://www.baidu.com",
+                "shortDesc": "",
+                "createdDate": 123
+            }
+        ],
+        "About me": [
+            {
+                "name": "About Me",
+                "src": "http://www.baidu.com",
+                "shortDesc": "",
+                "createdDate": 123
+            }
+        ]
+    };
 });
